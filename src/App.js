876abac0a48b002e897c5a532d02914ad872b945
Jim Brayton <github@jimbrayton.com>
2021-12-24 00:02:23 -0500

    Update App.js
    update URL

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { useState } from 'react'
 // A function to make requests to our Workers API using a query
 const getImages = async query => {
   // The base URL for our API
-  const url = "https://workers-unsplash-api.neutralgoods.workers.dev"
+  const url = "https://serverless-api.neutralgoods.workers.dev/"
 
   const resp = await fetch(url, {
     // Send a POST request
